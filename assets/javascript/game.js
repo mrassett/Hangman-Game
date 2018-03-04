8e0a4c86dedefeca747b73dcc86a575a23af9799
mrassett <magsrassett@gmail.com>
2018-03-04 15:35:17 -0600

    Hangman2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,57 @@
 "use strict";
 
 const wordBank =["voldemort", "quidditch", "ravenclaw", "snitch", "Lovegood", "azkaban", "dursley", "dumbledore", "minerva"]
-let randomWord = Math.floor (Math.random() * wordBank.length);
-let chosenWord = wordBank[randomWord];
-console.log(chosenWord);
+let numberofGuessesleft = 12; //displays how many guesses are left for player
+//Variables
+
+let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+
+//Elements
+
+// let wordChoice ;        // Lives
+// var counter ;           // Count correct geusses
+// var space;              // Number of spaces in word '-'
+
+
+//Generating random words
+let randomWord = Math.floor (Math.random() * wordBank.length); // Computer chooses random word
+let chosenWord = wordBank[randomWord];                         // Word being guessed
+let letterBlanks = [];                                         // Blank underscore
+
+// Blank spaces based on wordlength
+let wordChoice = () => {
+for (let i = 0; i < chosenWord.length; i++) {
+    letterBlanks.push('_'); 
+}
+return letterBlanks;
+
+} 
+
+// console.log(wordChoice());
+
+
+function updateDisplay(){
+    document.getElementById("numberofGuessesleft").innerText = numberofGuessesleft - 1;
+    document.getElementById("chosenWord").innerHTML=" ";
+    for(let i=0; i< rightLetter.length; i++) {
+        document.getElementById("chosenWord").innerText == guessedLetter[i];
+    }
+}
+
+document.keyPress = function(event) {
+   if (event.keyCode >= 65 && event.keyCode < 91);
+}
+// chosenWord.addEventListener = ("keypress") => {
+console.log("keypress");
+// }
+
+
+
+
 // alert(wordChoice);
 
 // let chosenWordLength = chosenWord.length() = 3;
