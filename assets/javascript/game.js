81f77e6e57d1393de9c69705f1fae53f2dd1585c
mrassett <magsrassett@gmail.com>
2018-03-04 10:44:05 -0600

    Hangman

---
+++ assets/javascript/game.js
@@ -0,0 +1,90 @@
+"use strict";
+
+const wordBank =["voldemort", "quidditch", "ravenclaw", "snitch", "Lovegood", "azkaban", "dursley", "dumbledore", "minerva"]
+let randomWord = Math.floor (Math.random() * wordBank.length);
+let chosenWord = wordBank[randomWord];
+console.log(chosenWord);
+// alert(wordChoice);
+
+// let chosenWordLength = chosenWord.length() = 3;
+// alert(chosenWordLength);
+// let dashWordLength = [];
+
+// for(let dashCounter = 0; dashCounter >= chosenWordLength; dashCounter++) {
+//     das
+// }
+
+
+
+
+
+
+// console.log (wordBank.word) 
+
+// function getLetterArray();
+
+
+
+// var words = [
+//     ""
+// ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+// let LetterArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+// let alphabet = getLetterArray();
+
+// function gameOver() {
+// 	if (letterBlanks.indexOf("_") === -1) {
+// 		wins++;
+// 		alert("winner winner");
+// 	} else {
+// 		losses++;
+// 		alert("lost")
+// 	}
+	
+// 	document.getElementById("winCount").innerHTML = wins;
+// 	document.getElementById("lossCount").innerHTML = losses;
+
+
+// // let wins = 0,
+// // losses = 0,
+// // correct = 0,
+// // lives = 8,
+// // lettersGuessed=[],
+// // letterBlanks=[],
+// // randomWord = "";
+// // let word = "timeup";
